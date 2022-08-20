541878bd7a21e27cd89db725150cd43ab8f533f0
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2022-08-20 20:32:23 +0200

    Resources

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .insert_resource(HelloTimer(Timer::from_seconds(1.0, false)))
+            .add_startup_system(add_people)
             .add_system(hello_world)
             .add_system(greet_people);
     }
@@ -23,8 +25,14 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-fn hello_world() {
-    println!("hello world!");
+struct GreetTimer(Timer);
+
+struct HelloTimer(Timer);
+
+fn hello_world(time: Res<Time>, mut timer: ResMut<HelloTimer>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        println!("hello world!");
+    }
 }
 
 fn add_people(mut commands: Commands) {
@@ -42,8 +50,10 @@ fn add_people(mut commands: Commands) {
         .insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0)
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0)
+        }
     }
 }
